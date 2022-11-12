f9977640585b89eceaed8067ff49c0def80b75e0
vikashtothenew <vikash.rai@tothenew.com>
2022-11-13 01:05:56 +0530

    v1

---
+++ src/main/java/com/spring/just/entity/Customer.java
@@ -0,0 +1,7 @@
+package com.spring.just.entity;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Customer {
+}
---
+++ src/main/java/com/spring/just/entity/User.java
@@ -0,0 +1,14 @@
+package com.spring.just.entity;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+
+@Entity
+@Getter
+@Setter
+public class User {
+
+}
