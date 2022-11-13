523a5a5a8b616310439ffa353e27420a80bf5895
vikashtothenew <vikash.rai@tothenew.com>
2022-11-13 10:45:22 +0530

    v2

--- src/main/java/com/spring/just/entity/User.java
+++ src/main/java/com/spring/just/entity/User.java
@@ -3,6 +3,7 @@ package com.spring.just.entity;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.http.converter.json.GsonBuilderUtils;
 
 import javax.persistence.Entity;
 
@@ -10,5 +11,8 @@ import javax.persistence.Entity;
 @Getter
 @Setter
 public class User {
-
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
