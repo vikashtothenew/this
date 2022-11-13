d457efb0a7cbe07fe30b8ecca04d692343098109
vikashtothenew <vikash.rai@tothenew.com>
2022-11-13 19:28:27 +0530

    do commit

--- src/main/java/com/spring/just/modal/News.java
+++ src/main/java/com/spring/just/modal/News.java
@@ -2,6 +2,8 @@ package com.spring.just.modal;
 
 public class News {
     public static void main(String[] args) {
-        System.out.println("Hi");
+        for(int i = 0; i <100;i++){
+            System.out.println("Hi");
+        }
     }
 }
